e25163b6be3c9c677a2df8f6ac91fd4eacaf15e2
Rob Halff <rob.halff@gmail.com>
2014-03-28 14:55:31 +0100

    flow authenticate

--- nodes/authenticate/node.js
+++ nodes/authenticate/node.js
@@ -1,1 +1,17 @@
-output = [passport, 'authenticate', input.strategy.name, input.options]
+//output = [passport, 'authenticate', input.strategy.name, input.options]
+output = function() {
+
+  passport.authenticate(
+    input.strategy.name,
+    input.options, function(err, user, info) {
+      cb({
+        error: err,
+        user: user,
+        info: info,
+        req: input.req,
+        res: input.res
+      });
+    }
+  )(input.req, input.res);
+
+};
